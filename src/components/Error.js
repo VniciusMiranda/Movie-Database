4458a9f152408c5ee975ad3618cad926c04be5b6
VniciusMiranda <viniciusmedeiros10.05@gmail.com>
2020-05-09 20:54:48 -0300

    add meassage error styling

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const Error = ({match}) =>{
+    console.log(match);
+    const {error} = match.params;
+    // there was no need to create a file just to add this
+    let style ={
+        color:'#fff',
+        textAlign: 'center',
+        marginTop: '80px',
+        padding: '10px',
+        fontSize: '25px'
+    }
+
     return (
-        <div>
-            <h1>{match.error} error</h1>           
+        <div style={style}>
+            <h1>{error} error</h1>
+            <p> try again with another search :/</p>           
         </div>
     );    
 }
